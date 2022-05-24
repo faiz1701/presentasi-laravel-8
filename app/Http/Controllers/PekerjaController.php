c2df85eb5d080378eef8e6f60ee8140f71f79264
faiz1701 <thefaiz17@gmail.com>
2022-05-24 23:05:25 +0700

    menghilangkan dd di update data

--- app/Http/Controllers/PekerjaController.php
+++ app/Http/Controllers/PekerjaController.php
@@ -7,35 +7,41 @@ use App\Models\pekerja;
 
 class PekerjaController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
         $data = pekerja::all();
         return view('datapekerja', compact('data'));
     }
 
-    public function tambahpekerja(){
+    public function tambahpekerja()
+    {
         return view('tambahdata');
     }
 
-    public function insertdata(Request $request){
+    public function insertdata(Request $request)
+    {
         pekerja::create($request->all());
         return redirect()->route('pekerja');
     }
 
-    public function tampilkandata($id){
+    public function tampilkandata($id)
+    {
 
         $data = pekerja::find($id);
         return view('editdata', compact('data'));
     }
 
-    public function updatedata(Request $request, $id){
-        dd($request->all());
+    public function updatedata(Request $request, $id)
+    {
+        // dd($request->all());
         $data = pekerja::find($id);
         $data->update($request->all());
         return redirect()->route('pekerja');
     }
 
-    public function deletedata($id){
+    public function deletedata($id)
+    {
         $data = pekerja::find($id);
         $data->delete();
         return redirect()->route('pekerja');
