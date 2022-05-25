c8cd8c122e5f27460693b31d9b54522869a4cf83
faiz1701 <thefaiz17@gmail.com>
2022-05-25 16:03:29 +0700

    edit route

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,18 @@ use App\Http\Controllers\PekerjaController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/pekerja',[PekerjaController::class, 'index'])->name('pekerja');
+Route::get('/', [PekerjaController::class, 'index'])->name('pekerja');
 
-Route::get('/tambahpekerja',[PekerjaController::class, 'tambahpekerja'])->name('tambahpekerja');
+Route::get('/tambahpekerja', [PekerjaController::class, 'tambahpekerja'])->name('tambahpekerja');
 
-Route::post('/insertdata',[PekerjaController::class, 'insertdata'])->name('insertdata');
+Route::post('/insertdata', [PekerjaController::class, 'insertdata'])->name('insertdata');
 
-Route::get('/tampilkandata/{id}',[PekerjaController::class, 'tampilkandata'])->name('tampilkandata');
+Route::get('/tampilkandata/{id}', [PekerjaController::class, 'tampilkandata'])->name('tampilkandata');
 
-Route::post('/updatedata/{id}',[PekerjaController::class, 'updatedata'])->name('updatedata');
+Route::post('/updatedata/{id}', [PekerjaController::class, 'updatedata'])->name('updatedata');
 
-Route::get('/deletedata/{id}',[PekerjaController::class, 'deletedata'])->name('deletedata');
+Route::get('/deletedata/{id}', [PekerjaController::class, 'deletedata'])->name('deletedata');
